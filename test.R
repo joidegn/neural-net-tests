020f2ec71286d5cdbbfe9f5ebc14afe4fd20c909
Johannes Degn <j@degn.de>
2014-01-13 11:32:42 +0100

    added predictions

--- test.R
+++ test.R
@@ -5,8 +5,11 @@ data(infert)
 nn.model <- neuralnet(case~age+parity+induced+spontaneous, data=infert, hidden=2, linear.output=FALSE, algorithm="backprop", err.fct="ce", act.fct="logistic", learningrate=0.01)
 nn.model2 <- neuralnet(case~age+parity+induced+spontaneous, data=infert, hidden=2, err.fct="ce", linear.output=FALSE, act.fct="logistic")
 
-plot(nn.model2)
-gwplot(nn.model2, selected.covariate="age")
+#plot(nn.model2)
+#gwplot(nn.model2, selected.covariate="age")
 
-prediction <- compute(nn.model2, covariate=matrix(c(22, 1, 1, 0), byrow=T, nrow=1))
-# prediction should be probability that a 22 year old of parity 1 with 1 induced abortion and 0 spontaneous abortions is infertile
+predictions <- compute(nn.model2, covariate=matrix(c(22, 1, 0, 0,
+                                                     22, 1, 1, 0,
+                                                     22, 1, 2, 0,
+                                                     22, 1, 3, 0), byrow=T, nrow=4))
+# prediction are predicted probabilities that a 22 year old of parity 1 with differing induced abortions and 0 spontaneous abortions is infertile
